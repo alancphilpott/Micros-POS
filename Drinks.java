6a61aac43265dcd04030dfe7db470f81177657af
alanp1995 <Alan.Philpott@students.ittralee.ie>
2016-11-19 11:21:11 +0000

    Created a Food and Drinks Class

---
+++ Drinks.java
@@ -0,0 +1,41 @@
+package Project;
+
+/**
+ * Created by Alan on 19/11/2016.
+ */
+public class Drinks {
+
+    private String drinkItemOne = "Tea";
+    private String drinkItemTwo = "Coffee";
+    private String drinkItemThree = "Hot Chocolate";
+    private double itemOnePrice = 2.00;
+    private double itemTwoPrice = 3.00;
+    private double itemThreePrice = 4.00;
+
+    public String getDrinkItemOne ()
+    {
+        return drinkItemOne;
+    }
+    public double getItemOnePrice ()
+    {
+        return itemOnePrice;
+    }
+
+    public String getDrinkItemTwo ()
+    {
+        return drinkItemTwo;
+    }
+    public double getItemTwoPrice ()
+    {
+        return itemTwoPrice;
+    }
+
+    public String getDrinkItemThree ()
+    {
+        return drinkItemThree;
+    }
+    public double getItemThreePrice ()
+    {
+        return itemThreePrice;
+    }
+}
---
+++ Food.java
@@ -0,0 +1,44 @@
+package Project;
+
+/**
+ * Created by Alan on 19/11/2016.
+ */
+public class Food {
+
+    private String foodItemOne = "Chicken Curry";
+    private String foodItemTwo = "Spaghetti Bolognese";
+    private String foodItemThree = "Steak Sandwich";
+    private double itemOnePrice = 10.00;
+    private double itemTwoPrice = 15.00;
+    private double itemThreePrice = 20.00;
+
+    public String getFoodItemOne ()
+    {
+        return foodItemOne;
+    }
+
+    public double getItemOnePrice ()
+    {
+        return itemOnePrice;
+    }
+
+    public String getFoodItemTwo ()
+    {
+        return foodItemTwo;
+    }
+
+    public double getItemTwoPrice ()
+    {
+        return itemTwoPrice;
+    }
+
+    public String getFoodItemThree ()
+    {
+        return foodItemThree;
+    }
+
+    public double getItemThreePrice ()
+    {
+        return itemThreePrice;
+    }
+}
