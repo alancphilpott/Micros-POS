273d9787986a256e7f05f17ab7fb053be1fb3b5a
alanp1995 <Alan.Philpott@students.ittralee.ie>
2016-11-19 12:49:18 +0000

    Created Welcome Label

--- Micros.java
+++ Micros.java
@@ -2,8 +2,6 @@
 
 package Project;
 
-import sun.text.resources.cldr.rn.FormatData_rn;
-
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
@@ -17,7 +15,7 @@ public class Micros extends JFrame
     private JLabel areaLabel; // Label To Name Each Food Item
     private JMenuBar menuBar; // To Create The Menu Bar
     private JMenu fileMenu;
-    private JMenu viewMenu; // Create The Menus For The Menu Bar
+    private JMenu billMenu; // Create The Menus For The Menu Bar
     private JMenuItem menuItem; // Create The Items For The Menus
     private Container cPane; // Container For All J Items
     private Food foodItem = new Food();
@@ -27,40 +25,55 @@ public class Micros extends JFrame
     public Micros ()
     {
         // Set The JFrame Properties ------------------------------------------------------------------
-        super("Micros System");
+        super("MicrosSys");
         setSize(230,430);
         setResizable(false);
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        setLocation(50,50);
+        setLocation(800,250);
 
         // ---------------------------------- Create The Container ----------------------------------
         cPane = getContentPane();
+        cPane.setBackground(Color.WHITE);
         cPane.setLayout(null);
+
+        // ---------------------------------- Create The Welcome/Instruction Label ----------------------------------
+        areaLabel = new JLabel("Welcome");
+        areaLabel.setFont(new Font("SansSerif", Font.BOLD, 20));
+        areaLabel.setLocation(70,5);
+        areaLabel.setSize(200,30);
+        cPane.add(areaLabel);
+        areaLabel = new JLabel("Choose What You Want");
+        areaLabel.setFont(new Font("SansSerif", Font.BOLD, 12));
+        areaLabel.setLocation(45,25);
+        areaLabel.setSize(200,30);
+        cPane.add(areaLabel);
 
         // ---------------------------------- Create The Food Label ----------------------------------
         areaLabel = new JLabel("Food Items");
         areaLabel.setFont(new Font("SansSerif", Font.BOLD, 20));
-        areaLabel.setLocation(10,10);
+        areaLabel.setForeground(Color.GRAY);
+        areaLabel.setLocation(10,50);
         areaLabel.setSize(200,30);
         cPane.add(areaLabel);
 
         // ---------------------------------- Create The Food Buttons ----------------------------------
         menuButton = new JButton(foodItem.getFoodItemOne() + " " + euro + foodItem.getItemOnePrice());
-        menuButton.setLocation(10,50);
+        menuButton.setLocation(10,90);
         menuButton.setSize(200,30);
         cPane.add(menuButton);
         menuButton = new JButton(foodItem.getFoodItemTwo() + " " + euro + foodItem.getItemTwoPrice());
-        menuButton.setLocation(10, 90);
+        menuButton.setLocation(10, 130);
         menuButton.setSize(200,30);
         cPane.add(menuButton);
         menuButton = new JButton(foodItem.getFoodItemThree() + " " + euro + foodItem.getItemThreePrice());
-        menuButton.setLocation(10,130);
+        menuButton.setLocation(10,170);
         menuButton.setSize(200,30);
         cPane.add(menuButton);
 
         // ---------------------------------- Create The Drinks Label ----------------------------------
         areaLabel = new JLabel("Food Items");
         areaLabel.setFont(new Font("SansSerif", Font.BOLD, 20));
+        areaLabel.setForeground(Color.GRAY);
         areaLabel.setLocation(10,210);
         areaLabel.setSize(200,30);
         cPane.add(areaLabel);
@@ -79,6 +92,18 @@ public class Micros extends JFrame
         menuButton.setSize(200,30);
         cPane.add(menuButton);
 
+        // Create The 'View' Menu To Hold Items
+        billMenu = new JMenu("Bill");
+
+        // Create Items To Add To 'View' Menu
+        menuItem = new JMenuItem("View Bill"); // New Menu Item Called View
+        menuItem.addActionListener(e -> JOptionPane.showMessageDialog(null,"You Choose To: View Bill")); // Action Listener For When 'View Bill' Is Clicked
+        billMenu.add(menuItem); // Add 'View Bill' To The Bill Menu
+
+        menuItem = new JMenuItem("Pay Bill");
+        menuItem.addActionListener(e -> JOptionPane.showMessageDialog(null,"You Choose To: Pay Bill"));
+        billMenu.add(menuItem);
+
         // Create The 'File' Menu To Hold Items
         fileMenu = new JMenu("File");
 
@@ -87,20 +112,11 @@ public class Micros extends JFrame
         menuItem.addActionListener(e -> System.exit(0)); // Action Listener For When 'Exit' Is Clicked
         fileMenu.add(menuItem); // Add 'Exit' To The File Menu
 
-        // Create The 'View' Menu To Hold Items
-        viewMenu = new JMenu("Bill");
-
-        // Create Items To Add To 'View' Menu
-        menuItem = new JMenuItem("View Bill"); // New Menu Item Called View
-        menuItem.addActionListener(e -> JOptionPane.showMessageDialog(null,"You Choose To: View Bill")); // Action Listener For When 'View Bill' Is Clicked
-        viewMenu.add(menuItem); // Add 'View Bill' To The Bill Menu
-
         // Create Menu Bar To Add Menus
         menuBar = new JMenuBar();
-        menuBar.add(fileMenu); menuBar.add(viewMenu);
+        menuBar.add(billMenu); menuBar.add(fileMenu);
 
         // Add The Menu Bar To The Frame
         setJMenuBar(menuBar);
     }
-
 }
