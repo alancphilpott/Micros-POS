0cc3977b29e0668e25f9c94a873502a7077bcb9c
alanp1995 <Alan.Philpott@students.ittralee.ie>
2016-11-19 11:56:32 +0000

    Food Label and Food Items Created

--- Micros.java
+++ Micros.java
@@ -12,51 +12,64 @@ public class Micros extends JFrame
     // Create Local Variables
 
     private JButton menuButton;  // Buttons For Adding Food
+    private JLabel areaLabel; // Label To Name Each Food Item
     private JMenuBar menuBar; // To Create The Menu Bar
     private JMenu fileMenu;
     private JMenu viewMenu; // Create The Menus For The Menu Bar
     private JMenuItem menuItem; // Create The Items For The Menus
     private Container cPane; // Container For All J Items
-    private Image foodImage; // For Adding Different Images To The Container
+    private Food foodItem = new Food();
+    private String euro = "\u20ac";
 
     public Micros ()
     {
         // Set The JFrame Properties
         super("Micros System");
-        setSize(1280,720);
-        setDefaultCloseOperation(EXIT_ON_CLOSE);
-        setLocation(0,0);
+        setSize(230,600);
+        setResizable(false);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setLocation(50,50);
 
         // Create The Container
         cPane = getContentPane();
-        cPane.setLayout(new GridBagLayout());
+        cPane.setLayout(null);
 
-        // Create The Menu To Hold Items
+        // Create The Food Label
+        areaLabel = new JLabel("Food Items");
+        areaLabel.setFont(new Font("SansSerif", Font.BOLD, 20));
+        areaLabel.setLocation(10,10);
+        areaLabel.setSize(200,30);
+        cPane.add(areaLabel);
+
+        // Create The Food Buttons
+        menuButton = new JButton(foodItem.getFoodItemOne() + " " + euro + foodItem.getItemOnePrice());
+        menuButton.setLocation(10,50);
+        menuButton.setSize(200,20);
+        cPane.add(menuButton);
+        menuButton = new JButton(foodItem.getFoodItemTwo() + " " + euro + foodItem.getItemTwoPrice());
+        menuButton.setLocation(10, 80);
+        menuButton.setSize(200,20);
+        cPane.add(menuButton);
+        menuButton = new JButton(foodItem.getFoodItemThree() + " " + euro + foodItem.getItemThreePrice());
+        menuButton.setLocation(10,110);
+        menuButton.setSize(200,20);
+        cPane.add(menuButton);
+
+        // Create The 'File' Menu To Hold Items
         fileMenu = new JMenu("File");
 
-        //Create Items To Add To File Menu
-        menuItem = new JMenuItem("Exit");
-        menuItem.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                System.exit(0);
-            }
-        });
-        fileMenu.add(menuItem);
+        //Create Items To Add To 'File' Menu
+        menuItem = new JMenuItem("Exit"); // New Menu Item Called Exit
+        menuItem.addActionListener(e -> System.exit(0)); // Action Listener For When 'Exit' Is Clicked
+        fileMenu.add(menuItem); // Add 'Exit' To The File Menu
 
-        // Create View Menu
+        // Create The 'View' Menu To Hold Items
         viewMenu = new JMenu("Bill");
 
-        // Create Items To Add To View Menu
-
-        menuItem = new JMenuItem("View");
-        menuItem.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                JOptionPane.showMessageDialog(null,"You Choose To: View Bill");
-            }
-        });
-        viewMenu.add(menuItem);
+        // Create Items To Add To 'View' Menu
+        menuItem = new JMenuItem("View Bill"); // New Menu Item Called View
+        menuItem.addActionListener(e -> JOptionPane.showMessageDialog(null,"You Choose To: View Bill")); // Action Listener For When 'View Bill' Is Clicked
+        viewMenu.add(menuItem); // Add 'View Bill' To The Bill Menu
 
         // Create Menu Bar To Add Menus
         menuBar = new JMenuBar();
