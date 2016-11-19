ca189816930d65f750ca58a5b37d8d3193072d4c
alanp1995 <Alan.Philpott@students.ittralee.ie>
2016-11-19 10:21:22 +0000

    Commit 1

--- Micros.java
+++ Micros.java
@@ -7,19 +7,31 @@ import java.awt.*;
 
 public class Micros extends JFrame
 {
-    private JButton homeButton;
-    private JMenuBar menuBar;
+    // Create Local Variables
+
+    private JButton menuButton;  // Buttons For Adding Food
+    private JMenuBar menuBar; // To Create The Menu Bar
     private JMenu fileMenu;
-    private JMenu viewMenu;
-    private JMenuItem menuItem;
-    private Container cPane;
+    private JMenu viewMenu; // Create The Menus For The Menu Bar
+    private JMenuItem menuItem; // Create The Items For The Menus
+    private Container cPane; // Container For All J Items
+    private Image foodImage; // For Adding Different Images To The Container
 
     public Micros ()
     {
+        // Set The JFrame Properties
         super("Micros System");
         setSize(1280,720);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setLocation(0,0);
+
+        // Create The Container
+        cPane = getContentPane();
+        cPane.setLayout(new GridBagLayout());
+
+        // Create The Menu To Hold Items
+
+        
 
     }
 
