2b6699e20840d52ef739ddf856138811189e2f03
Alan Philpott <Alan.Philpott@students.ittralee.ie>
2016-12-05 09:38:49 +0000

    Javadoc comments added to Bill.java

--- Bill.java
+++ Bill.java
@@ -2,18 +2,26 @@ import javax.swing.*;
 import java.awt.*;
 import java.util.ArrayList;
 
-/** Created by Alan on 19/11/2016 */
+/** Date Created: 19/11/2016.
+ * This class is used to create a bill */
 
 public class Bill {
 
-    private static double totalSales;
-    static double outAmt;
-    private double billTotal;
-    private ArrayList<Meal> mealList = new ArrayList<>();
-    private ArrayList<Meal> allMeals = new ArrayList<>();
+    private static double totalSales; // Keeps track of the total of all sales that took place.
+    static double outAmt; // Keeps track of the total outstanding amount to be paid.
+    private double billTotal; // Stores the total amount for each bill created.
+    private ArrayList<Meal> mealList = new ArrayList<>(); // An ArrayList of type Meal to store all meals added to a Bill.
+    private ArrayList<Meal> allMeals = new ArrayList<>(); // An ArrayList of type Meal to store all meals added to every Bill created.
 
+    /**
+     * Default Constructor.
+     */
     public Bill () { }
 
+    /**
+     * Add's a meal to a Meal ArrayList, Increments the total of a Bill, Increments the total of all Bills, Increments the outstanding amount of all Bills.
+     * @param meal Meal Type
+     */
     public void setBill(Meal meal)
     {
         mealList.add(meal);
@@ -22,26 +30,46 @@ public class Bill {
         outAmt += meal.getPrice();
     }
 
+    /**
+     * Adds's a meal to a Meal ArrayList.
+     * @param meal Meal Type
+     */
     public void setAllMeals (Meal meal)
     {
         allMeals.add(meal);
     }
 
+    /**
+     * This method is used to get the total of all sales.
+     * @return The total of all sales that occurred.
+     */
     public static double getTotalSales ()
     {
         return totalSales;
     }
 
+    /**
+     * This method is used to get the outstanding amount of all unpaid Bills.
+     * @return The total outstanding amount in all unpaid Bills.
+     */
     public static double getOutAmt ()
     {
         return outAmt;
     }
 
+    /**
+     * This method returns the cost of a Bill.
+     * @return The total cost of a Bill.
+     */
     public double getBillTotal()
     {
         return this.billTotal;
     }
 
+    /**
+     * This method returns a Bill with a list of all items on the Bill.
+     * @return A JTextArea with a list of all meals on a bill.
+     */
     public JTextArea getBillList()
     {
         String billFormat = "";
@@ -61,6 +89,10 @@ public class Bill {
         return billArea;
     }
 
+    /**
+     * This method returns a Bill with a list of all items sold in a session.
+     * @return A JTextArea with a list of all meals sold in a session.
+     */
     public JTextArea getAllMeals ()
     {
         String billFormat = "";
